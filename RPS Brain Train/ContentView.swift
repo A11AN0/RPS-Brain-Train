da276d551194f88f3f028c3436f3d1ea35f5613d
Allan <matrixboston7613@gmail.com>
2023-01-23 23:30:55 +0800

    initial commit, short description commented

--- RPS Brain Train/ContentView.swift
+++ RPS Brain Train/ContentView.swift
@@ -2,7 +2,16 @@
 //  ContentView.swift
 //  RPS Brain Train
 //
-//  Created by Allan Odunga on 23/1/2023.
+//  Created by A11AN0
+/*
+ RPS Brain Train (rock-paper-scissors) brain training
+ is a brain teaser app which asks players to either win,
+ lose or draw at a game of rock paper scissors. It will have a
+ set number of rounds (likely 10ish) and will be times. The more rounds the user
+ goes through, the faster the timer will run - giving the user less time to select
+ a response, and really stimulating the brain lol.
+ 
+ */
 //
 
 import SwiftUI
