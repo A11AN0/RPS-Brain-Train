2a23bab38aa73e0219708a47e88c5336b41964d1
Allan <matrixboston7613@gmail.com>
2023-01-23 23:23:17 +0800

    Initial Commit

---
+++ RPS Brain Train/RPS_Brain_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_Brain_TrainApp.swift
+//  RPS Brain Train
+//
+//  Created by Allan Odunga on 23/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct RPS_Brain_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
