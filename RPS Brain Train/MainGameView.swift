3cfdcc7e3b46ff52bd095b855dfd658e5b279e0a
Allan <matrixboston7613@gmail.com>
2023-01-29 01:47:15 +0800

    formatting and also - moved readyToPlay from GameTimer to parent so that timer will only begin countdown upon the player tapping ready

--- RPS Brain Train/MainGameView.swift
+++ RPS Brain Train/MainGameView.swift
@@ -18,59 +18,78 @@ struct MainGameView: View {
     @State private var shuffledSingleItem = itemChoice.shuffled()[0]
     @State private var numberOfRounds = 1;
     @State private var timeRemaining = 30;
+    @State private var readyToPlay = false; /*moved to parent so timer will start only when GameTimer button is clicked */
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
+    
     func resetRound() {
         numberOfRounds += 1
         shuffledItemsArr = MainGameView.itemChoice.shuffled()
         shuffledResultsArr = MainGameView.randomResult.shuffled()
         shuffledSingleItem = MainGameView.itemChoice.shuffled()[0]
     }
+    
 
     var body: some View {
         VStack {
-            Spacer()
-            GameTimer(timeRemainder: timeRemaining)
-                .onReceive(timer){ _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                    }
-                }
-            Text("\(shuffledResultsArr[0].uppercased())")
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .frame(maxWidth: .infinity)
-                .padding(5)
-                .background(Color(.systemGray5))
-            Spacer()
-            VStack {
-                SquareTile(size: 180)
-                Text(shuffledSingleItem.uppercased())
-                    .font(.title)
-            }
-            Spacer()
-            HStack {
-                ForEach(0..<3){number in
-                    Button {
-                        resetRound()
-                    } label: {
-                        VStack {
-                            SquareTile(size: 110)
-                            Text(shuffledItemsArr[number].uppercased())
-                                .font(.title3)
-                            
+            VStack(spacing: 0) {
+                /*
+                 needed to use binding for readyToPlay state, as it
+                is modified by GameTimer - kinda cool - because this is
+                 how children views can modify the state of their parent views
+                 */
+                
+                GameTimer(isReady: $readyToPlay, timeRemainder: timeRemaining)
+                    .onReceive(timer){ _ in
+                        if timeRemaining > 0 && readyToPlay {
+                            timeRemaining -= 1
                         }
                     }
-                    .buttonStyle(PlainButtonStyle())
+                
+                if readyToPlay {
+                    Text("\(shuffledResultsArr[0].uppercased())")
+                        .font(.largeTitle)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity)
+                        .padding(5)
+                        .background(Color(.systemGray5))
                 }
+                
             }
-            Spacer()
-            Text("\(numberOfRounds)/\(maxRounds)")
-                .font(.largeTitle)
-                .fontWeight(.light)
-            Spacer()
+            
+            if readyToPlay {
+                Spacer()
+                VStack {
+                    SquareTile(size: 180)
+                    Text(shuffledSingleItem.uppercased())
+                        .font(.title)
+                }
+                Spacer()
+                HStack {
+                    ForEach(0..<3){number in
+                        Button {
+                            resetRound()
+                        } label: {
+                            VStack {
+                                SquareTile(size: 110)
+                                Text(shuffledItemsArr[number].uppercased())
+                                    .font(.title3)
+                                
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+                Spacer()
+                Text("\(numberOfRounds)/\(maxRounds)")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                Spacer()
+            }
+            
         }
           .navigationBarBackButtonHidden(true)
+          
     }
 }
 
