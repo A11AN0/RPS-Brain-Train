f32f54022c96c8d8d4fe285751885b9ce06aeb8d
Allan <matrixboston7613@gmail.com>
2023-02-12 00:23:24 +0800

    score passed down to score view and prompt square tile views

--- RPS Brain Train/ScoreView.swift
+++ RPS Brain Train/ScoreView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct ScoreView: View {
+    var restartFunc: () -> Void
+    var score: Int
+   //correct way to pass functions down from parents
+    @State private var isVisible = false
+    
     var body: some View {
-        Text("The score goes here")
-    }
+        VStack {
+            Spacer()
+            Text("YOUR SCORE")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .frame(maxWidth: .infinity, maxHeight: 120)
+                .scaleEffect(1.3)
+
+            Text("\(score)")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .frame(maxWidth: .infinity, maxHeight: 120)
+                .scaleEffect(2)
+            //will need to put restart image here
+            Spacer()
+            Text("PLAY AGAIN?")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .scaleEffect(1.1)
+            HStack {
+                Spacer()
+                VStack {
+                    SquareTile()
+                    Text("YES")
+                        .font(.title)
+                        .fontWeight(.light)
+                    
+                        
+                }
+                .scaleEffect(0.8)
+                .onTapGesture {
+                    restartFunc()
+                }
+                Spacer()
+                VStack {
+                    SquareTile()
+                    Text("NO")
+                        .font(.title)
+                        .fontWeight(.light)
+                }
+                .scaleEffect(0.8)
+                Spacer()
+                //go back to title
+                
+            }
+            Spacer()
+        }
+        .opacity(isVisible ? 1 : 0)
+        .onAppear {
+            withAnimation {
+                self.isVisible = true
+            }
+            
+        }
+    }   
+    
 }
 
 struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreView()
+        //will need to pass through the score here as a binding
+        // will need to
+        ScoreView( restartFunc: {}, score: 14)
     }
 }
